fc15cbe0b71d1fe58e3221d22d82742b7ec72d5d
srsandy <sandeepranjan2007@yahoo.co.in>
2018-06-21 19:20:47 +0530

    background process added to monitor URL's

--- script.js
+++ script.js
@@ -1,103 +1,17 @@
 /*
 * API File
 */
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./lib/config');
-const fs = require('fs');
-const handlers = require('./lib/handlers');
-const helpers = require('./lib/helpers');
 
-helpers.sendTwilloSms('7077620252', 'hi', err => {
-  console.log(err);
-});
+const server = require('./lib/server');
+const workers = require('./lib/workers');
 
-//HTTP SERVER
-const httpServer = http.createServer((req, res) => {
-  unifiedServer(req, res);
-});
+const app = {};
 
-httpServer.listen(config.httpPort, ()=> {
-  console.log(`Server on port ${config.httpPort}`);
-});
+app.init = () => {
+  server.init();
+  workers.init();
+}
 
-//HTTPS SERVER
-const httpsServerOptions = {
-  'key': fs.readFileSync('./https/key.pem'),
-  'cert': fs.readFileSync('./https/cert.pem')
-};
+app.init();
 
-const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
-  unifiedServer(req, res);
-});
-
-httpsServer.listen(config.httpsPort, ()=> {
-  console.log(`Server on port ${config.httpsPort}`);
-});
-
-const unifiedServer = (req, res) => {
-  // Get the URL and parse it
-  var parsedUrl = url.parse(req.url, true);
-
-  // Get the path
-  var path = parsedUrl.pathname;
-  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-  //Get the query string as an object
-  var queryStringObject = parsedUrl.query;
-
-  //Get the HTTP Method
-  var method = req.method.toLowerCase();
-
-  //Get the headers
-  var headers = req.headers;
-
-  //Get the payloads
-  var decoder = new StringDecoder('utf-8');
-  var buffer = '';
-  req.on('data', data => {
-    buffer += decoder.write(data);
-  });
-
-  req.on('end', () => {
-    buffer += decoder.end();
-
-    //choose the handler this req should go to
-    var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-    //Construct the data object to send to the handler
-    var data = {
-      trimmedPath,
-      queryStringObject,
-      method,
-      headers,
-      payload: helpers.parseJsonToObject(buffer)
-    };
-
-    //route the request to the handler specifed request
-    chosenHandler(data, (statusCode, payload) => {
-      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-      payload = typeof(payload) == 'object' ? payload : {};
-
-      //convert payload to string
-      var payloadString = JSON.stringify(payload);
-
-      //return the response
-      res.setHeader('Content-Type', 'application/json');
-      res.writeHead(statusCode);
-      res.end(payloadString);
-
-      console.log(buffer);
-    });
-  });
-};
-
-//define a request router
-var router = {
-  ping: handlers.ping,
-  users: handlers.users,
-  tokens: handlers.tokens,
-  checks: handlers.checks
-};
+module.exports = app;
